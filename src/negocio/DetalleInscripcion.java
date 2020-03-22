ff27def616e076cf4ecc60d1481c8b6f44f0189c
vanrz <jpmrsguan@gmail.com>
2020-03-22 17:02:24 -0500

    pruebaaa

--- src/negocio/DetalleInscripcion.java
+++ src/negocio/DetalleInscripcion.java
@@ -11,4 +11,32 @@ package negocio;
  */
 public class DetalleInscripcion {
     
+    private int k_identificacion;
+    private int k_ins;
+    private int k_familiar;
+
+    public int getK_identificacion() {
+        return k_identificacion;
+    }
+
+    public void setK_identificacion(int k_identificacion) {
+        this.k_identificacion = k_identificacion;
+    }
+
+    public int getK_ins() {
+        return k_ins;
+    }
+
+    public void setK_ins(int k_ins) {
+        this.k_ins = k_ins;
+    }
+
+    public int getK_familiar() {
+        return k_familiar;
+    }
+
+    public void setK_familiar(int k_familiar) {
+        this.k_familiar = k_familiar;
+    }
+    
 }
